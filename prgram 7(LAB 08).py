46061bd712b60f05da033ff120f2b5aa18e8c1a4
AbdulMoiz22 <45107763+AbdulMoiz22@users.noreply.github.com>
2019-01-05 20:47:25 +0500

    Add files via upload

---
+++ prgram 7(LAB 08).py
@@ -0,0 +1,10 @@
+print('Abdul Moiz')
+print('18b-011-CS')
+print('Lab # 08')
+print ('12/28/2018')
+print ('Question 7')
+def cheers(n):
+    again_string =' '.join(n)
+    print ('How do you spell winner?\nI know!, I know!\n',again_string.upper(),'\nAnd that\'s how you spell winner Go' , n.title(),'!')
+cheers('uitians')
+
